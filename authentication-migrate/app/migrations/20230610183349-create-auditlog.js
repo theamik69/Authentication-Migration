8be9323c3287bec36fb5ac68e7f7395fce61e3b0
theamik69 <amishbah12@gmail.com>
2023-06-11 01:47:20 +0700

    add auditlog table

---
+++ authentication-migrate/app/migrations/20230610183349-create-auditlog.js
@@ -0,0 +1,33 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Auditlogs', {
+      id: {
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      table_name: {
+        type: Sequelize.STRING
+      },
+      task: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Auditlogs');
+  }
+};
--- authentication-migrate/app/models/user.js
+++ authentication-migrate/app/models/user.js
@@ -33,6 +33,21 @@ module.exports = (sequelize, DataTypes) => {
     email: DataTypes.STRING,
     password: DataTypes.STRING
   }, {
+    hooks: {
+      afterCreate: async (user, option) => {
+        console.log('>> user afterCreate', sequelize?.models);
+        // query insert into logs
+        try {
+          await sequelize.models.Auditlog.create({
+            table_name: 'Users',
+            task: 'insert',
+            description: `Proses Insert dengan data ${JSON.stringify(user.toJSON())}`,
+          });
+        } catch (e) {
+          console.log('>> error user afterCreate', e);
+        }
+      },
+    },
     sequelize,
     modelName: 'User',
   });
